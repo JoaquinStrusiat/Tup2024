099e468824b933f3f3021c4ce55df9fe6fe2dbc4
Joaquin Strusiat <joaquinpjs@gmail.com>
2024-08-22 01:27:01 -0300

    22/8/2024 - 1:26

--- Programacion2/src/Practica/SupermercadoPOO/Clases/Producto.java
+++ Programacion2/src/Practica/SupermercadoPOO/Clases/Producto.java
@@ -1,4 +1,5 @@
 package Practica.SupermercadoPOO.Clases;
+import java.util.ArrayList;
 
 public class Producto {
     private String nombre;
@@ -21,6 +22,15 @@ public class Producto {
         return cantidad;
     }
     public String toString() {
-        return "Producto: " + nombre + ", Precio: $" + String.format("%.2f", precio) + ", Cantidad: " + cantidad + " unidades";
+        return "Producto: " + nombre + ", Precio: $" + String.format("%.2f", precio) + ", Unidades disponibles: " + cantidad;
+    }
+
+    public  ArrayList<Producto> getProductos(){
+        ArrayList<Producto> productos = new ArrayList<>();
+        productos.add(new Producto("CocaCola", 1800, 100));
+        productos.add(new Producto("Sprite", 1500, 50));
+        productos.add(new Producto("Alfajor Tatin", 600, 50));
+        productos.add(new Producto("Caramelos Masticables", 20.50, 200));
+        return productos;
     }
 }
--- Programacion2/src/Practica/SupermercadoPOO/Main.java
+++ Programacion2/src/Practica/SupermercadoPOO/Main.java
@@ -1,9 +1,38 @@
 package Practica.SupermercadoPOO;
 import Practica.SupermercadoPOO.Clases.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+
+            //Aca se crea a un comentiro :3 
+            Empleado empleado1 = new Empleado("Federico", "Aranda", 12345678, 100000);
+            Empleado empleado2 = new Empleado("Joaquin", "Strusiat", 87654321, 100000);
+            
+            // Para simplificar el codigo vamos a crear manualmente 4 cajas
+            Caja caja1 = new Caja(empleado1, 1);
+            Caja caja2 = new Caja(empleado2, 2);
+            Caja caja3 = new Caja(empleado1, 3);
+            Caja caja4 = new Caja(empleado2, 4);
+            
+            System.out.println("--------Inicio de compra--------");
+            System.out.print("Ingrese su Nombre: ");
+            String nombre = sc.nextLine();
+            System.out.print("Ingrese su Apellido: ");
+            String apellido = sc.nextLine();
+            System.out.print("Ingrese su DNI: ");
+            int dni = sc.nextInt();
+            System.out.print("Para compra mayorista ingrese 1, de lo contrario 0: ");
+            int bandera = sc.nextInt();
+            boolean mayorista = false;  
+            if ( bandera == 1){
+                mayorista = true;
+            }
+            //Creamos el nuevo Cliente
+            Cliente cliente1 = new Cliente(nombre, apellido, dni, mayorista);
+            
+        sc.close();
     }
     
 }
